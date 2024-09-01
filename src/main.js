982e9d23678f8420b9730ab768b5c9c0cdb6d3be
Falguni Mondal <career.falguni@gmail.com>
2024-09-02 04:32:46 +0530

    Bug fixes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import SmoothScrolling from './components/utils/SmoothScrolling'
+import SmoothScrolling from './components/utils/SmoothScrolling.jsx'
 
 createRoot(document.getElementById('root')).render(
     <SmoothScrolling>
