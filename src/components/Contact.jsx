d13052b724eadbd19c0c897592b5c8223f23b32d
Falguni Mondal <career.falguni@gmail.com>
2024-09-03 11:07:08 +0530

    Contact form is now working.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,10 +9,10 @@ const Contact = () => {
     <section id='contact' className='w-full mt-[15vh]'>
       <SectionHeading heading={'Connect.'} />
       <div className="contact-here flex flex-col gap-20 mt-[5vh]">
-        <form className='w-full flex flex-col gap-8' action="">
+        <form className='w-full flex flex-col gap-8' name='connect' netlify >
           <input className='w-full outline-none py-2 bg-transparent border-b-2 border-[#f8f8f8] placeholder:text-[#f8f8f8]' placeholder='Your email' type="email" />
           <textarea className='w-full outline-none bg-transparent border-b-2 border-[#f8f8f8] placeholder:text-[#f8f8f8]' placeholder='Message' rows={6} name="message" id="connnect-message"></textarea>
-          <button className='text-[#242329] bg-[#27DFB3] font-medium py-2.5 w-[45vw] rounded-full'>Send Message</button>
+          <button type='submit' className='text-[#242329] bg-[#27DFB3] font-medium py-2.5 w-[45vw] rounded-full'>Send Message</button>
         </form>
         <div className="contact-dets-sontainer flex flex-col gap-8">
           <div className='contact-dets flex flex-col'>
