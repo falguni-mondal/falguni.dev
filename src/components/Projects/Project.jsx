b68629c3281d7e4875f1fb46a240de1a04cc574a
Falguni Mondal <career.falguni@gmail.com>
2024-09-05 23:54:06 +0530

    Bug fixes

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -9,7 +9,7 @@ const Project = (props) => {
   return (
     <li onMouseEnter={projectRevealer} onMouseLeave={projectUnRevealer} className='project relative w-full 2xl:h-[25vh] lg:h-[32vh] flex flex-col justify-center lg:border-b'>
       <a href={link} target='_blank' className='w-full'>
-        <img className='project-img w-full lg:w-[20vw] scale-90 opacity-0 rounded-xl object-cover lg:absolute lg:top-0 lg:left-[60%]' src={img} alt="Obys Agency" />
+        <img className='project-img w-full lg:w-[20vw] lg:scale-90 lg:opacity-0 rounded-xl object-cover lg:absolute lg:top-0 lg:left-[60%]' src={img} alt="Obys Agency" />
         <div className='w-full'>
           <h3 className='font-semibold text-[6.5vw] lg:text-[3vw] tracking-tight flex items-center gap-5'>{name} <IoIosLink /></h3>
           <div className='flex justify-between lg:mb-[5vh]'>
